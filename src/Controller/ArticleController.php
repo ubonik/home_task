0e95d312ca91aa7f0b22b91784ce39e0c51b81c9
Редкошеев Александр <liders05@mail.ru>
2020-10-11 15:11:52 +0500

    домашнее задание Doctrine ORM

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,74 +2,40 @@
 
     namespace App\Controller;
     use App\Homework\ArticleContentProviderInterface;
+    use App\Repository\ArticleRepository;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\Routing\Annotation\Route;
 
     class ArticleController extends AbstractController
     {
-        protected $articles = [
-            ['title' => 'Что делать, если надо верстать?', 'image' => 'images/article-2.jpeg',
-                'author' => 'Хэтээмэль Цеэсесович', 'clock' => '3 дня назад', 'comment' => 5,
-                'src' => 'https://robohash.org/хэтээмэль_цеэсесович.png?set=set3',
-                'badgies' => ['Html', 'Css']
-            ],
-
-            ['title' => 'Facebook ест твои данные', 'image' => 'images/article-1.jpeg',
-                'author' => 'Not Facebook', 'clock' => '26 дней назад', 'comment' => 0,
-                'src' => 'https://robohash.org/not_facebook.png?set=set3',
-                'badgies' => ['Facebook']
-            ],
-
-            ['title' => 'Когда пролил кофе на клавиатуру', 'image' => 'images/article-3.jpg',
-                'author' => 'Поливайкин', 'clock' => '1 месяц назад', 'comment' => 300,
-                'src' => 'https://robohash.org/поливайкин.png?set=set3',
-                'badgies' => ['Балбес', 'Криворучка', 'Рукопоп']
-            ],
-            ['title' => 'Что делать, если надо верстать?', 'image' => 'images/article-2.jpeg',
-                'author' => 'Хэтээмэль Цеэсесович', 'clock' => '3 дня назад', 'comment' => 5,
-                'src' => 'https://robohash.org/хэтээмэль_цеэсесович.png?set=set3',
-                'badgies' => ['Html', 'Css']
-            ],
-            ['title' => 'Когда пролил кофе на клавиатуру', 'image' => 'images/article-3.jpg',
-                'author' => 'Поливайкин', 'clock' => '1 месяц назад', 'comment' => 300,
-                'src' => 'https://robohash.org/поливайкин.png?set=set3',
-                'badgies' => ['Балбес', 'Криворучка', 'Рукопоп']
-            ]
-        ];
-
-        /**
+         /**
          * @Route("/", name = "app_homepage")
          */
-        public function homepage()
+        public function homepage(ArticleRepository $repository)
         {
-            return $this->render('/article/homepage.html.twig', ['articles' => $this->articles]);
+            $articles = $repository->findLatestPublished();
+            foreach ($articles as $article) {
+                if (!empty($article->getKeywords())) {
+                    $keywords = explode(',', $article->getKeywords());
+                }
+            }
+            return $this->render('/article/homepage.html.twig', ['articles' => $articles,'keywords' => $keywords ]);
         }
 
         /**
          * @Route("/article/{slug}", name = "app_article_show")
          */
-        public function show($slug, ArticleContentProviderInterface $articleContentProvider)
+        public function show($slug, ArticleContentProviderInterface $articleContentProvider, ArticleRepository $repository)
         {
-
-            if (isset($this->articles[$slug])) {
-
-                /**
-                 * выбор случайного слова $word из массива $words
-                 */
-                $words = ['кофе', 'молоко', 'чай', 'сок', 'вода', 'какао', 'пепси'];
-
-                $num = rand(1, 10);
-                $word = ($num <= 7) ? $words[array_rand($words)] : null;
-
-                $wordsCount = ($num <= 7) ? rand(5, 30) : 0;
-
-                $articleContent = $articleContentProvider->get(rand(2, 10), $word, $wordsCount);
-
-                return $this->render('article/detail.html.twig', ['article' => $this->articles[$slug],
-                    'articleContent' => $articleContent]);
-
+            $article = $repository->findOneBy(['slug' => $slug]);
+            if (!$article) {
+                throw $this->createNotFoundException(sprintf('статья: %s не найдена', $slug));
             }
+
+            $keywords = explode(',', $article->getKeywords());
+
+                return $this->render('article/detail.html.twig', ['article' => $article, 'keywords' => $keywords]) ;
         }
 
         /**
@@ -84,7 +50,6 @@
             $content = $contentProvider->get($paragrafs, $word,  $count);
 
             return $this->render('articles/article_content/index.html.twig', ['content' => $content]);
-
         }
 
     }
---
+++ src/Repository/ArticleRepository.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Article;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Article|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Article|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Article[]    findAll()
+ * @method Article[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ArticleRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Article::class);
+    }
+
+       /**
+        * @return Article[] Returns an array of Article objects
+        */
+
+    public function findLatestPublished()
+    {
+        $queryBuilder = $this->findPublished($this->findLatest());
+
+        return $queryBuilder;
+    }
+    /**
+     * @return Article[] Returns an array of Article objects
+     */
+    public function findLatest()
+    {
+            return  $this->latest()
+                ->getQuery()
+                ->getResult()
+            ;
+    }
+
+    /**
+     * @return Article[] Returns an array of Article objects
+     */
+
+    public function findPublished()
+    {
+
+        return $this->published()
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    private function published(QueryBuilder $db = null)
+    {
+       return $this->getOrCreateQueryBuilder($db)->andWhere('a.publishedAt IS NOT NULL');
+    }
+
+    private function latest(QueryBuilder $db = null)
+    {
+        return $this->getOrCreateQueryBuilder($db)->orderBy('a.publishedAt', 'DESC');
+    }
+
+
+
+    /*
+    public function findOneBySomeField($value): ?Article
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+    private function getOrCreateQueryBuilder(?QueryBuilder $db = null): QueryBuilder
+    {
+        return $db ?? $this->createQueryBuilder('a');
+    }
+
+}
